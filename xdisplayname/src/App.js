2775d025642564e00e7f2359ac431c36c0f8ff7f
santhoshwesley <santhoshwesleyyer@gmail.com>
2024-02-22 13:19:45 +0530

    displayname

---
+++ xdisplayname/src/App.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [fullName, setFullName] = useState("");
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    if (name === "firstName") {
+      setFirstName(value);
+    } else {
+      setLastName(value);
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (firstName && lastName) {
+      setFullName(`${firstName} ${lastName}`);
+    }
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <div className="input-container">
+          <label htmlFor="firstName">First Name: </label>
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            value={firstName}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="input-container">
+          <label htmlFor="lastName">Last Name: </label>
+          <input
+            type="text"
+            id="lastName"
+            name="lastName"
+            value={lastName}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      {fullName && <div>Full Name: {fullName}</div>}
+    </div>
+  );
+}
+
+export default App;
